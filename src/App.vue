aded00404c19f689eeb5521678eaaa501ca84a3f
LighuenR <lighuen.restanio@gmail.com>
2021-07-07 13:44:22 -0300

    Data fetch

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,38 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+   <main class="main">
+    <v-card max-width="800"> 
+      <h1> {{weatherData.name}} </h1> 
+    </v-card>
+   </main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
   data: () => ({
-    //
+    weatherData: {}
   }),
+
+  mounted() {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=Cordoba&appid=8de24b65852e6b354d3e60e84a5a209b')
+      .then((res) => res.json())
+      .then(data => {
+      this.weatherData = data;
+      console.log("Esto es weather info", this.weatherData)
+      })
+  }
 };
 </script>
+
+<style scoped>
+.main { display: grid;
+        place-items: center;
+        background: grey;
+        min-height: 100vh;
+        color: white;
+     }
+
+</style>
